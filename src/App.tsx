29ddae9ff8fd30754a465f38adcc1f83799f49ba
shivansh <shivneeraj2004@gmail.com>
2024-01-28 22:49:30 +0530

    todo with typescript complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
-import { useContext, useRef } from "react";
+import { useRef } from "react";
 import "./App.css";
-import { todoContext } from "./context/TodoContext";
+import { useTodoContext } from "./context/TodoContext";
 // import Todo from "./models/todos";
 import { v4 as uuid } from "uuid";
-
+import { FiX } from "react-icons/fi";
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { MdOutlineCheckBox } from "react-icons/md";
 function App() {
-  const { todos, setTodos } = useContext(todoContext);
+  const { todos, setTodos, checked, setChecked } = useTodoContext();
 
   const inpref = useRef<HTMLInputElement>(null);
 
   function handleSubmit(evt: React.FormEvent) {
     evt.preventDefault();
-    setTodos([...todos, { id: uuid(), item: inpref.current!.value }]);
+    setTodos([
+      ...todos,
+      { id: uuid(), item: inpref.current!.value, isChecked: false },
+    ]);
   }
+
+  function handleTodos(id: string) {
+    setTodos(todos.filter((el) => el.id !== id));
+  }
+
+  function handleCheck(id: string) {
+    setTodos(
+      todos.map((el) => {
+        if (id === el.id) {
+          setChecked(!checked);
+          return { ...el, isChecked: checked };
+        }
+        // setChecked(!checked);
+        return { ...el };
+
+        // return {...el,isChecked:true}
+      })
+    );
+  }
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -20,8 +45,31 @@ function App() {
         <button>+</button>
       </form>
       <ul>
-        {value?.todos.map((el) => {
-          return <li>{el.item}</li>;
+        {todos.map((el) => {
+          return (
+            <div>
+              <button
+                onClick={() => {
+                  handleCheck(el.id);
+                }}
+              >
+                {el.isChecked ? (
+                  <MdOutlineCheckBox></MdOutlineCheckBox>
+                ) : (
+                  <MdCheckBoxOutlineBlank></MdCheckBoxOutlineBlank>
+                )}
+              </button>
+              <li
+                key={el.id}
+                className={`${el.isChecked && "line-through opacity-50"}`}
+              >
+                {el.item}
+              </li>
+              <button onClick={() => handleTodos(el.id)}>
+                <FiX></FiX>
+              </button>
+            </div>
+          );
         })}
       </ul>
     </div>
--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,8 +1,8 @@
-import { Children, ReactNode, createContext } from "react";
+import { Children, ReactNode, createContext, useContext } from "react";
 import Todo from "../models/todos";
 import { useState } from "react";
 
-export const todoContext = createContext<Value>(null!);
+export const todoContext = createContext<Value | null>(null);
 
 interface myProp {
   children: ReactNode;
@@ -10,16 +10,31 @@ interface myProp {
 
 interface Value {
   todos: Todo[];
-  setTodos(prev: Todo[]): void;
+  checked: boolean;
+  // setTodos(prev: Todo[]): void;
+  // setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setTodos: (prev: Todo[]) => void;
+  setChecked: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function TodoContextData({ children }: myProp): JSX.Element {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [checked, setChecked] = useState<boolean>(true);
 
   const value: Value = {
     todos,
     setTodos,
+    checked,
+    setChecked,
   };
 
   return <todoContext.Provider value={value}>{children}</todoContext.Provider>;
+}
+
+export function useTodoContext() {
+  const context = useContext(todoContext);
+  if (context === null) {
+    throw new Error("useTodoContext must be used within a TodoContextData");
+  }
+  return context;
 }
